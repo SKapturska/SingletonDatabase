54d1929bc657d2f3debaa11f02704ca4b74562e7
SKapturska <skapturska@us.edu.pl>
2016-11-27 14:43:58 +0100

    Koniec

--- src/singletondatabase/SingletonConnection.java
+++ src/singletondatabase/SingletonConnection.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package singletondatabase;
 
-/**
- *
- * @author Sandi
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SingletonConnection {
+    
+    private static SingletonConnection instance = null;
+    String URL = "jdbc:mysql://127.0.0.1/addressbook?user=root&password=haslo";
+    Connection conn;
+    Statement st;
+    
+    private SingletonConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(URL);
+            st = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    public static SingletonConnection getInstance(){
+        if(instance == null){
+            instance = new SingletonConnection();
+        }
+        return instance;
+    }
+    
+    ResultSet executeQuery(String query){
+        try {
+            return  st.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    } 
+    
+    int executeUpdate(String query){
+        try {
+            return st.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
     
 }
--- src/singletondatabase/SingletonDatabase.java
+++ src/singletondatabase/SingletonDatabase.java
@@ -1,21 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package singletondatabase;
 
-/**
- *
- * @author Sandi
- */
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class SingletonDatabase {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       menu();
+        
+        
+    }
+    
+    static void menu(){
+        SingletonConnection singletonConnection = SingletonConnection.getInstance();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1: wyświetlanie tablicy \n2: aktualizacja tablicy\n");
+        int choice = sc.nextInt();
+        
+        if(choice == 1){ 
+            try {
+            ResultSet rs = singletonConnection.executeQuery("Select * from person");
+            while(rs.next()){
+            System.out.println("id: " + rs.getString(1));
+            System.out.println("nazwisko: " + rs.getString(2));
+            System.out.println("imie: " + rs.getString(3));
+            System.out.println("miasto: " + rs.getString(4));
+            System.out.println("nr telefonu: " + rs.getString(5));
+            System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+            
+        }else if(choice == 2){
+            Scanner sc2 = new Scanner(System.in);
+            System.out.println("Podaj komendę SQL: ");
+            //sc.next();
+            String query = sc2.nextLine();
+            System.out.println(singletonConnection.executeUpdate(query));
+        }
     }
     
 }
